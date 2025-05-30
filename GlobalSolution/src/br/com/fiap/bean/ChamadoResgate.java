cec2b247304f1762e24767d1aca9905243826a23
Isis Macedo <89617847+isismodd@users.noreply.github.com>
2025-05-30 20:01:18 -0300

    Update ChamadoResgate.java

--- GlobalSolution/src/br/com/fiap/bean/ChamadoResgate.java
+++ GlobalSolution/src/br/com/fiap/bean/ChamadoResgate.java
@@ -3,9 +3,10 @@ package br.com.fiap.bean;
  * @author Isis Macedo
  * @version 5.0
  */
-public class ChamadoResgate{
+public class ChamadoResgate extends Ong{
 //atributos
   private String tipoDeResgate;
+  private int idResgate;
   
 
 }
