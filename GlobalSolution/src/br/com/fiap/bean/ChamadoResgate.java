e8bc97f364f00122836c95b527929ccf675b4765
Isis Macedo <89617847+isismodd@users.noreply.github.com>
2025-05-30 19:59:46 -0300

    Update ChamadoResgate.java

--- GlobalSolution/src/br/com/fiap/bean/ChamadoResgate.java
+++ GlobalSolution/src/br/com/fiap/bean/ChamadoResgate.java
@@ -1,1 +1,11 @@
+package br.com.fiap.bean;
+/**Classe para cadastrar empresas parceiraas e listar
+ * @author Isis Macedo
+ * @version 5.0
+ */
+public class ChamadoResgate{
+//atributos
+  private String tipoDeResgate;
+  
 
+}
